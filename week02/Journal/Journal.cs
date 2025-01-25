a61595fe5b1ad66fd16414ae131d22bded724108
nsisong-byui <akp24023@byui.edu>
2025-01-25 16:47:25 +0100

    Journal program

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,76 @@
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+public class Journal
+{
+    public List<Entry> Entries = new();
+    public string _fileName;
+
+    public void DisplayEntries(bool edit = false) {
+        if (edit == true)
+        {
+            int index = 1;
+            foreach (Entry e in Entries)
+            {
+            Console.WriteLine($"{index}. {e._date} - {e._prompt}\n{e._answer}\n");
+            index++;
+            }
+        }
+        else
+        {
+            foreach (Entry e in Entries)
+            {
+            Console.WriteLine($"{e._date} - {e._prompt}\n{e._answer}\n");
+            }
+        }
+        
+    }
+
+    public void EditEntry() {
+        Console.Write("Which entry would you want to update? (0 to return) ");
+        int entryIndex = int.Parse(Console.ReadLine()) -1;
+
+        if (entryIndex >= 0)
+        {
+        Console.WriteLine($"\n{Entries[entryIndex]._date} - {Entries[entryIndex]._prompt}");
+        string newAnswer = Console.ReadLine();
+
+        Entries[entryIndex]._answer = newAnswer;
+        }
+        else if (entryIndex == 0) {}
+        else {}
+    }
+
+    public void LoadFromFile() {
+        Console.Write("Please enter filename to load the journal: ");
+        string fileName = Console.ReadLine();
+
+        Console.WriteLine("Reading journal from file...");
+        string[] lines = System.IO.File.ReadAllLines(fileName);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("||");
+
+            Entry newEntry = new();
+            newEntry._answer = parts[2];
+            newEntry._date = parts[0];
+            newEntry._prompt = parts[1];
+
+            Entries.Add(newEntry);
+        }
+    }
+
+    public void SaveToFile() {
+        Console.Write("Please enter filename to save the journal: ");
+        string fileName = Console.ReadLine();
+
+        Console.WriteLine("Saving journal to file...");
+        using(StreamWriter outputfile = File.AppendText(fileName))
+        {
+            foreach (Entry e in Entries)
+            {
+                outputfile.WriteLine($"{e._date}||{e._prompt}||{e._answer}");
+            }
+        }
+
+    }
+}
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,53 @@
+// To exceed requirements, I created a new option for the user to edit an entry.
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new();
+        Entry entry = new();
+        PromptGenerator promptGenerator = new();
+
+        bool repeat = true;
+
+        Console.WriteLine("My Journal Program menu");
+        while (repeat) {
+            Console.WriteLine("Journal Program Menu:");
+            Console.WriteLine("\n1. Write a new entry");
+            Console.WriteLine("\n2. Display the journal");
+            Console.WriteLine("\n3. Save the journal to a file");
+            Console.WriteLine("\n4. Load the journal from a file");
+            Console.WriteLine("\n5. Edit Entries"); // Creativity 
+            Console.WriteLine("\n6. Exit");
+            Console.Write(" Please enter your choice: ");
+            int selectedNum = int.Parse(Console.ReadLine());
+
+
+            if (selectedNum == 1) {
+                string randomPrompt = promptGenerator.GenerateRandomPrompt();
+                Console.WriteLine(randomPrompt);
+
+                journal.Entries.Add(entry.GenerateNewEntry(randomPrompt));
+              }
+              else if (selectedNum == 2) {
+                journal.DisplayEntries();
+              }
+              else if (selectedNum == 3) {
+                journal.LoadFromFile();
+              }
+              else if (selectedNum == 4) {
+                journal.SaveToFile();
+              }
+              else if (selectedNum == 5) { //Creativity line
+                journal.DisplayEntries(true);
+                journal.EditEntry();
+              }
+              else if (selectedNum == 6) {
+                repeat = false;
+              }
+        }
     }
 }
